29209810e1404dde97bda40ad95f2c3390e85ec6
KenHuynh0610 <nhan.huynh06@gmail.com>
2020-11-02 23:38:29 -0800

    built message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_071127) do
+ActiveRecord::Schema.define(version: 2020_11_03_072412) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
