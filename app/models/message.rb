4d7c216b66a1e80dfc66af6dd68ea0b04337851a
KenHuynh0610 <nhan.huynh06@gmail.com>
2020-11-04 00:22:48 -0800

    completed message rendering

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,12 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :content, presence: true
+    def show
+    
+    end
+
+    def index
+        @messages = Message.all.recent
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'session#new'
+  resources :messages, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
