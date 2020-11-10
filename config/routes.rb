6b8f87d01fad45d75e897e66e655034511f61653
KenHuynh0610 <nhan.huynh06@gmail.com>
2020-11-09 23:46:30 -0800

    finished signup page with form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
-  get 'login', to: 'session#new'
+  get 'login', to: 'session#login'
   resources :messages, only: [:show]
+  get'signup', to: 'users#new'
+  resources :users, expect: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
